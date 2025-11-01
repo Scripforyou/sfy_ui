76c30dd5fabe618f0f818bd26e42a378621ce07d
Scripforyou <anitotech25@gmail.com>
2025-11-01 01:03:37 -0700

    Create item_details.lua

---
+++ item_details.lua
@@ -0,0 +1,30 @@
+-- Optional: detailed object info (for debugging and accurate targeting)
+local itemDetails = {
+    Frostling = {
+        Path = "game.Workspace.Items.Frostling.Hitbox",
+        Name = "Hitbox",
+        Class = "Part",
+        Parent = "Frostling",
+        Position = Vector3.new(6.01, 370.17, -96.02),
+        Size = Vector3.new(1.77, 0.53, 3.92),
+        Anchored = false,
+        CanCollide = false,
+        Transparency = 1,
+        Material = Enum.Material.Plastic,
+        Color = Color3.fromRGB(223, 223, 222)
+    },
+
+    Log = {
+        Path = "game.Workspace.Items.Log.PrimaryPart",
+        Name = "PrimaryPart",
+        Class = "Part",
+        Parent = "Log",
+        Position = Vector3.new(58.38, 370.71, 10.18),
+        Size = Vector3.new(4.03, 2.25, 1.47),
+        Anchored = false,
+        CanCollide = true,
+        Transparency = 0,
+        Material = Enum.Material.Wood,
+        Color = Color3.fromRGB(108, 88, 75)
+    }
+}
